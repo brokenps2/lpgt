0f1e6aeb3804276eb1c75683f608067715d98e90
ELI <feeson88@gmail.com>
2024-01-18 14:59:11 -0600

    add handling in the case of a missing .obj file

--- src/OBJLoader.cpp
+++ src/OBJLoader.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -8,6 +9,19 @@ string getModelSrc(string path) {
   ifstream in(path);
   string contents((istreambuf_iterator<char>(in)),
     istreambuf_iterator<char>());
+
+  if(contents == "") {
+    char ans;
+    std::cout << "\n";
+    std::cerr << "Specified OBJ File: " << path << " couldn't be read! (or the file is empty)" << std::endl;
+    std::cout << "continue running program with empty file? y/N" << std::endl;
+    std::cin >> ans;
+    if(ans == 'y' || ans == 'Y') {
+
+    } else {
+      exit(0);
+    }
+  }
 
   return contents;
 
