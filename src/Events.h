2c4ed1a7b371b850035bf4e23842ed7cad372af2
eli <feeson88@gmail.com>
2024-02-28 19:32:45 -0600

    add glfw events and try to fix window size on tiling wms

--- src/Events.h
+++
@@ -1,6 +0,0 @@
-#include <SDL2/SDL.h>
-#pragma once
-
-int checkForQuit();
-
-int isKeyDown(const int sdlk);
---
+++ src/Input.cpp
@@ -0,0 +1,5 @@
+#include <GLFW/glfw3.h>
+#include "WindowManager.h"
+
+void initInput() {
+}
